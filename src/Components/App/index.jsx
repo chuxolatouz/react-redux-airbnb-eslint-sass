02352994fbf4fabd751e58f23ce65e2df3bf5e84
Jesus Rincon <00jjessejj00@gmail.com>
2018-05-04 11:02:00 -0400

    First commit

---
+++ src/Components/App/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AppPropType from '../../PropTypes/AppPropType';
+import logo from './logo.svg';
+import './index.css';
+
+class App extends Component {
+  componentWillMount() {
+    /* eslint-disable no-console */
+    console.log('someThing');
+    /* eslint-enable no-console */
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+App.propTypes = { ...AppPropType };
+export default App;
---
+++ src/Reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+// add reducers in alphabetical order
+const rootReducer = combineReducers({
+  //
+});
+
+export default rootReducer;
